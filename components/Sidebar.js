9e6b9eeb1dbb9988ab1b659e9093668bb0a077fa
Asad Memon <amdeveloping0908@gmail.com>
2022-11-28 19:41:32 +0530

    added localstorage functionality

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "../styles/Sidebar.module.css";
 import { useSelector } from "react-redux";
+import { getWithExpiry, setWithExpiry } from "../helpers/localstorageTTL";
 
 const SidebarLists = ({ itemname }) => {
   return (
@@ -12,6 +13,53 @@ const SidebarLists = ({ itemname }) => {
 };
 
 const Sidebar = () => {
+  const [sidebarValues, setSidebarValues] = useState([]);
+  const sidebarValueRef = useRef();
+
+  useEffect(() => {
+    const val = getWithExpiry("sidebarValues");
+    if (val != null) {
+      setSidebarValues(val);
+    }
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("storage", () => {
+      setSidebarValues(getWithExpiry("sidebarValues"));
+      console.log("change");
+    });
+    return () => {
+      window.removeEventListener("storage", () => {});
+    };
+  }, []);
+
+  const addItemToSidebar = (e) => {
+    const itemObj = {
+      id: Math.random(),
+      title: sidebarValueRef.current.value,
+      url: "",
+      response: "",
+    };
+
+    if (e.key == "Enter") {
+      const currentValues = getWithExpiry("sidebarValues");
+      if (currentValues && currentValues.length > 4) {
+        alert("You cant add more than 5 api endpoints...");
+        return;
+      }
+      if (currentValues == null) {
+        const items = [];
+        items.push(itemObj);
+        setWithExpiry("sidebarValues", items, 1000 * 60 * 60);
+      } else {
+        currentValues.push(itemObj);
+        setWithExpiry("sidebarValues", currentValues, 1000 * 60 * 60);
+      }
+      window.dispatchEvent(new Event("storage"));
+      sidebarValueRef.current.blur();
+    }
+  };
+
   const sidebarState = useSelector((state) => state.sidebar.value);
   const [addItem, setAddItem] = useState(false);
   return (
@@ -24,13 +72,15 @@ const Sidebar = () => {
         Add +
       </button>
       <div className={styles.sidebarLists}>
-        <SidebarLists itemname="Item a" />
-        <SidebarLists itemname="Item b" />
-        <SidebarLists itemname="Item c" />
+        {sidebarValues?.map((item) => (
+          <SidebarLists key={item.id} itemname={item.title} />
+        ))}
       </div>
       {addItem && (
         <div className={styles.sidebarAddList}>
           <input
+            ref={sidebarValueRef}
+            onKeyDown={addItemToSidebar}
             onBlur={() => setAddItem(false)}
             autoFocus={true}
             type="text"
